ac42c4cbee791a180bf6987733aec22e011d1499
juliet93 <103969545+juliet93@users.noreply.github.com>
2022-04-18 20:56:12 -0400

    github sucks big time

--- src/gitproject/project1.java
+++ src/gitproject/project1.java
@@ -5,6 +5,12 @@ public class project1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("finally got it");
+
+
+System.out.println("finally got it");
+System.out.println("finally got it");
+System.out.println("finally got it");
+
 	}
 
 }
